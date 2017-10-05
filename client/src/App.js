c62a9a942a2f1697319073b0cafc3a1d846be627
Philip Marazita <pmarazita@gmail.com>
2017-10-04 20:34:34 -0700

    Add redux-form dependcy and outline test form for handling input values

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,23 @@ import {
   View,
   Text
 } from 'react-native'
+import AddItem from './components/AddItem';
 
 
 class App extends React.Component {
   constructor() {
     super()
   }
-
+  
+  handleSubmit(values) {
+    console.log('help me', values);
+  }
+  
   render () {
     return (
       <View>
         <Text>This is App</Text>
+        <AddItem onSubmit={this.handleSubmit} />
       </View>
     )
   }
--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -7,23 +7,51 @@ import {
   TextInput,
   Button,
 } from 'react-native'
+import { Field, reduxForm, formValueSelector } from 'redux-form'
 
-import * as itemActions from '../../actions/itemActions.js'
+import * as itemActions from '../actions/itemActions';
+import CustomTextInput from './CustomTextInput';
 
 
 class AddItem extends React.Component {
 
+  testing() {
+    console.log('Also in here', this.props.testinput)
+  }
+
   render () {
     return ( 
-      <View >
-        <Text></Text>
+      <View>
+        <View>
+          <Text>Test input</Text>
+          <Field name='testinput' component={CustomTextInput} />
+        </View>
+        <View>
+          <Text>{'Here is your input: ' + this.props.testinput}</Text>
+        </View>
+        <View>
+          <Button
+            onPress={(values) => {
+              this.props.handleSubmit(values);
+              this.testing(values);
+              }}
+            title='Submit' 
+          />
+        </View>
       </View>
     )
   }
 }
 
+AddItem = reduxForm({
+  form: 'addItem'
+})(AddItem);
+const selector = formValueSelector('addItem');
+
 const AddItemState = (store) => {
+  const testinput = selector(store, 'testinput');
   return {
+    testinput,
     username: store.auth.username,
     fridge: store.fridge.fridge,
   }
