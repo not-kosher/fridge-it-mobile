562f165d5213bb739fc094f2247eb3305680c3d9
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-05 12:15:24 -0700

    Login form gets from input value from redux

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,32 +1,79 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { reduxForm, Field, formValueSelector } from 'redux-form';
+import * as authActions from '../actions/authActions.js';
 
 import {
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
 
 import CustomTextInput from './CustomTextInput.js';
 
+const Login = (props) => {
+  console.log('here are the props: ', props);
 
-const Login = (props) => (
-  <View>
+  return (
     <View>
-      <Text>Email</Text>
-      <Field 
-        name={'email'}
-        component={CustomTextInput}
-      />
-    </View>
+      <View>
+        <Text>Email</Text>
+        <Field 
+          name={'email'}
+          component={CustomTextInput}
+          placeholder='email'
+        />
+      </View>
 
-    <View>
-      <Text>Password</Text>
-      <Field 
-        name={'password'}
-        component={CustomTextInput}
-      />
-    </View>
-  </View>
-);
+      <View>
+        <Text>Password</Text>
+        <Field 
+          name={'password'}
+          component={CustomTextInput}
+          placeholder='password'
+        />
+      </View>
 
-export default reduxForm({ form: 'signIn' })(Login);
+      <View>
+        <Button 
+          onPress={() => props.handleSubmit(props.emailValue, props.passwordValue)}
+          title='sign-up'
+        >
+          Sign Up
+        </Button>
+
+        <Button 
+          onPress={() => props.handleSubmit(props.emailValue, props.passwordValue)}
+          title='login'
+        >
+          Login
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+Login = reduxForm({
+  form: 'login'
+})(Login);
+
+const LoginState = (store) => {
+  const selector = formValueSelector('login');
+  const emailValue = selector(store, 'email');
+  const passwordValue = selector(store, 'password');
+  return {
+    emailValue,
+    passwordValue,
+    username: store.auth.username,
+    fridge: store.fridge.fridge,
+  }
+};
+
+const LoginDispatch = (dispatch) => {
+  return {
+    itemActions: bindActionCreators(authActions, dispatch)
+  }
+};
+
+export default connect(LoginState, LoginDispatch)(Login);
