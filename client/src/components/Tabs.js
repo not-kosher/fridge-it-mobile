b4d8aada242310517a9fa559b5ee6fe4bc321baf
Philip Marazita <pmarazita@gmail.com>
2017-10-07 10:48:09 -0700

    Add files for tab view and messages

---
+++ client/src/components/Tabs.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button, Platform, ScrollView, StyleSheet } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import SampleText from './SampleText';
+
+import Fridge from './Fridge';
+import MessageView from './MessageView';
+
+Fridge.navigationOptions = {
+  tabBarLabel: 'My Fridge',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <Ionicons
+      name={focused ? 'ios-home' : 'ios-home-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  ),
+};
+
+MessageView.navigationOptions = {
+  tabBarLabel: 'Messages',
+  tabBarIcon: ({ tintColor, focused }) => (
+    <Ionicons
+      name={focused ? 'ios-chatboxes' : 'ios-chatboxes-outline'}
+      size={26}
+      style={{ color: tintColor }}
+    />
+  ),
+};
+
+const Tabs = TabNavigator(
+  {
+    Home: {
+      screen: Fridge,
+      path: '',
+    },
+    Messages: {
+      screen: MessageView,
+      path: 'messages',
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? '#e91e63' : '#fff',
+    },
+  }
+);
+
+export default Tabs;
